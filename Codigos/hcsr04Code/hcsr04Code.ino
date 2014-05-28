eb3ec7b55f30379791e5ef5e95ec1cda2aa7e9b7
unknown <hscholzem@gmail.com>
2014-05-28 12:31:38 -0300

    Aprimoramento do algoritmo de verificação da distância.

--- Codigos/hcsr04Code/hcsr04Code.ino
+++ Codigos/hcsr04Code/hcsr04Code.ino
@@ -3,37 +3,53 @@
 #define TRIGGER_PIN  12 
 #define ECHO_PIN     13
 
-int accuracy = 100;
-
 Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
 
-float distance = 0;
-int i = 0;
+const int ACCURACY = 50; // Define a precisão do sensor. Quanto maior acurracy, maior precisão porem menor velocidade de atualização da posição
+float samples[ACCURACY];
+int index = 0; // Indice do sample
+boolean was = false;
+
+int distance()
+{
+  float cmFromWall;
+  long microsec = ultrasonic.timing();
+  cmFromWall = ultrasonic.convert(microsec, Ultrasonic::CM);
+  
+  float sum = 0;
+  
+  if( was )
+  {
+    samples[ index%ACCURACY ] = cmFromWall;
+    index++;
+  }
+  else
+  {
+    for( int i = 0; i < ACCURACY; i++ )
+    {
+      samples[i] = cmFromWall;
+    }
+    was = true;
+  }
+  for( int i = 0; i < ACCURACY; i++ )
+  {
+     sum += samples[ i ];
+  }
+  
+  return (int) (sum/ACCURACY);
+}
 
 void setup()  
 { 
   pinMode( TRIGGER_PIN, OUTPUT );
   pinMode( ECHO_PIN, INPUT );
   Serial.begin(9600);
+  Serial.println( "Start" );
 }
 
 void loop()  
 {
-  if( i < accuracy )
-  {
-    float cmFromWall;
-    long microsec = ultrasonic.timing();
-    cmFromWall = ultrasonic.convert(microsec, Ultrasonic::CM);
-    distance += cmFromWall;
-    i++;
-  }
-  
-  else
-  {
-    distance = (int)(distance / accuracy);
-    Serial.println( distance );
-    distance = 0;
-    i = 0;
-  }
+    Serial.print( "Distance: " );
+    Serial.println( distance() );
 }
 
