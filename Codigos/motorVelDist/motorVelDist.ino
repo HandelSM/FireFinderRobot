98433b47dd4de5668f4c9665d71bc876fc4399a5
unknown <hscholzem@gmail.com>
2014-07-23 13:14:23 -0300

    Código para desviar de paredes

--- Codigos/motorVelDist/motorVelDist.ino
+++ Codigos/motorVelDist/motorVelDist.ino
@@ -1,13 +1,15 @@
 //HCSR04 lib and pins
-#include <Ultrasonic.h>
+#include <NewPing.h>
+
 #define TRIGGER_PIN  12
 #define ECHO_PIN     13
+#define MAX_DISTANCE 150
 
 //Motor pins
-#define MOTOR_L      10
-#define MOTOR_R       9
+int motorR[2] = { 5, 6 };
+int motorL[2] = { 9, 10 };
 
-Ultrasonic ultrasonic( TRIGGER_PIN, ECHO_PIN );
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 typedef struct motor
 {
@@ -17,50 +19,47 @@ typedef struct motor
 };
 typedef struct motor Motor;
 
-Motor rightMotor = { 0, 255, 130 }; // Motor nomeMotor = { velocidade, velocidadeMaxima, velocidadeMinima }
-
-Motor leftMotor = { 0, 240, 120 };
-
 float distance()
 {
-  float cmFromWall;
-  long microsec = ultrasonic.timing();
-  cmFromWall = ultrasonic.convert( microsec, Ultrasonic::CM );
+  delay(30);                      // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
+  unsigned int uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
+  //Serial.print("Ping: ");
+  float cmFromWall = uS / US_ROUNDTRIP_CM; // Convert ping time to distance in cm and print result (0 = outside set distance range)
   return cmFromWall;
-}
-
-void updateVel( float d, struct motor m )
-{
-  float vel = d * m.maxVel/200; // Para ter a velocidade máxima, precisa estar a 200cm, ou mais, da parede.
-  
-  if( vel < m.minVel && d > 20 )
-  {
-    vel = m.minVel;
-  }
-  
-  if( vel > m.maxVel )
-  {
-    vel = m.maxVel;
-  }
-  
-  m.vel = vel;
 }
 
 void setup()
 {
-  pinMode( MOTOR_R, OUTPUT );
-  pinMode( MOTOR_L, OUTPUT );
+  pinMode( TRIGGER_PIN, OUTPUT );
+  pinMode( ECHO_PIN, INPUT );
+  
+  pinMode( motorR[0], OUTPUT );
+  pinMode( motorR[1], OUTPUT );
+  pinMode( motorL[0], OUTPUT );
+  pinMode( motorL[1], OUTPUT );
   Serial.begin(9600);
 }
 
 void loop()  
-{ 
-  Serial.println( distance() );
+{  
+  float gap = distance();
   
-  updateVel( distance(), rightMotor );
-  updateVel( distance(), leftMotor );
+  Serial.println( gap );
   
-  analogWrite( MOTOR_R, rightMotor.vel );
-  analogWrite( MOTOR_L, leftMotor.vel );                       
+  
+  if( gap > 20 || gap == 0 )
+  {
+    analogWrite( motorR[0], 255 );
+    analogWrite( motorR[1], 0 );
+    analogWrite( motorL[0], 255 );
+    analogWrite( motorL[1], 0 );                       
+  }
+  else
+  {
+    analogWrite( motorR[0], 0 );
+    analogWrite( motorR[1], 250 );
+    analogWrite( motorL[0], 250 );
+    analogWrite( motorL[1], 0 );   
+  }
 }
 
